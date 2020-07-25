4899d6ede3ebb965fed3acbd3ab032cd48295cd2
Salim Oussayfi <box@slimou.net>
2020-07-25 18:22:45 +0200

    added controller

---
+++ spring5webapp/src/main/java/slimou/springframework/spring5webapp/controller/BookController.java
@@ -0,0 +1,28 @@
+package slimou.springframework.spring5webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import slimou.springframework.spring5webapp.domain.repository.BookRepository;
+
+/**
+ * User: salim
+ * Date: 25.07.2020 18:15
+ */
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
